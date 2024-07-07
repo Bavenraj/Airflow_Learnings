118657ef9d25f7f379689df45ab47bca695af1f4
BAVENRAJ <bavenraj08@gmail.com>
2024-07-07 23:16:35 +0800

    Airflow Third Commit

--- dags/simple_dag_example.py
+++ dags/simple_dag_example.py
@@ -1,26 +1,26 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-import datetime as dt
+from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'Bavenraj',
-    'start_date': dt.datetime(2024,8,10),
     'retries': 5,
-    'retry_delay': dt.timedelta(minutes=1),
+    'retry_delay': timedelta(minutes=1),
 }
 
 with DAG(
     dag_id = 'simple_example',
     description = 'A simple DAG',
     default_args = default_args,
-    schedule_interval = "@daily", 
+    start_date = datetime(2024, 6, 30, 2), # run 2 am
+    schedule_interval = "@daily" 
 ) as dag:
     task1 = BashOperator(
         task_id = 'print_hello',
-        bash_command = 'echo \'Greetings. the date and time are \'',
+        bash_command = 'echo \'Greetings. the date and time are \''
     )
     task2 = BashOperator(
         task_id = 'print_date',
-        bash_command = 'date',
+        bash_command = 'date'
     )
     task1 >> task2
